4ed29674513d88d7fda717466ef86c2ef6f3d6e6
daniel0076 <gfes1094@gmail.com>
2014-08-10 00:54:24 +0800

    add tabindex

--- apply/index.php
+++ apply/index.php
@@ -54,16 +54,16 @@ function registFailed() {
 			<div id="frm" class="frm">
 				<div class="four fields">
 					<div class="field">
-						<label>姓名</label>
-						<input name="name" placeholder="請輸入真實姓名" type="TEXT">
+						<label >姓名</label>
+						<input name="name" placeholder="請輸入真實姓名" type="TEXT" tabindex="1">
 					</div>
 					<div class="five wide field">
-						<label>生日</label>
-						<input name="birthday" placeholder="YYYY/MM/DD，ex:1995/03/23" type="TEXT">
+						<label >生日</label>
+						<input name="birthday" placeholder="YYYY/MM/DD，ex:1995/03/23" type="TEXT" tabindex="2">
 					</div>
 					<div class="three wide field">
 						<label>我是</label>
-						<div class="ui dropdown selection">
+						<div class="ui dropdown selection" tabindex="3">
 								<input name='gender' type="hidden">
 								<div class="default text">...</div>
 								<i class="dropdown icon"></i>
@@ -75,73 +75,73 @@ function registFailed() {
 					</div>
 					<div class="field">
 						<label>學號</label>
-						<input name="studentID" placeholder="請輸入學號" type="TEXT">
+						<input name="studentID" placeholder="請輸入學號" type="TEXT" tabindex="4">
 					</div>
 				</div>
 				<div class="two fields">
 					<div class="four wide field">
 							<label>身份證字號</label>
-							<input name="idnum" placeholder="首位大寫，A123456789" type="TEXT">
+							<input name="idnum" placeholder="首位大寫，A123456789" type="TEXT" tabindex="5">
 					</div>
 					<div class="twelve wide field">
 							<label>E-mail</label>
-							<input name="email" placeholder="請輸入email" type="TEXT">
+							<input name="email" placeholder="請輸入email" type="TEXT" tabindex="6">
 					</div>
 				</div>
 				<div class="two fields">
 						<div class="four wide field">
 								<label>手機</label>
-								<input name="cellphone" placeholder="0912-345678" type="TEXT">
+								<input name="cellphone" placeholder="0912-345678" type="TEXT" tabindex="7">
 						</div>
 						<div class="twelve wide field">
 								<label>地址</label>
-								<input name="address" placeholder="你家住哪啊?" type="TEXT">
+								<input name="address" placeholder="你家住哪啊?" type="TEXT" tabindex="8">
 						</div>
 				</div>
 				<div class="four fields">
 						<div class="four wide field">
 								<label>家裡電話</label>
-								<input name="telephone" placeholder="03-5712121" type="TEXT">
+								<input name="telephone" placeholder="03-5712121" type="TEXT" tabindex="9">
 						</div>
 						<div class="four wide field">
 								<label>緊急聯絡人</label>
-								<input name="emergency_cont" placeholder="" type="TEXT">
+								<input name="emergency_cont" placeholder="" type="TEXT" tabindex="10">
 						</div>
 						<div class="three wide field">
 								<label>關係</label>
-								<input name="relation" placeholder="" type="TEXT">
+								<input name="relation" placeholder="" type="TEXT" tabindex="11">
 						</div>
 						<div class="five wide field">
 								<label>緊急聯絡人電話</label>
-								<input name="emergency_tel" placeholder="0912-345678" type="TEXT">
+								<input name="emergency_tel" placeholder="0912-345678" type="TEXT" tabindex="12">
 						</div>
 				</div>
 				<div class="five fields">
 						<div class="four wide field">
 							<label>血型(特殊血請註明)</label>
-							<input name="bloodtype" placeholder="A/B/AB/O/其它" type="TEXT">
+							<input name="bloodtype" placeholder="A/B/AB/O/其它" type="TEXT" tabindex="13">
 						</div>
 						<div id="food" class="three wide field">
 							<label>飲食習慣</label>
 								<div class="ui radio checkbox">
-									<input name="food" value="meat" type="RADIO">
+									<input name="food" value="meat" type="RADIO" tabindex="14">
 									<label>肉食性</label>
 								</div>
 						</div>
 						<div class="two wide field">
 								<label><br></label>
 								<div class="ui radio checkbox">
-									<input name="food" value="veg" type="RADIO">
+									<input name="food" value="veg" type="RADIO" tabindex="15">
 									<label>草食性</label>
 								</div>
 						</div>
 						<div class="five wide field">
 							<label>畢業高中</label>
-							<input name="graduation" placeholder="ex：師大附中/台中女中" type="TEXT">
+							<input name="graduation" placeholder="ex：師大附中/台中女中" type="TEXT" tabindex="16">
 						</div>
 					<div class="two wide field">
 						<label>營服尺寸</label>
-						<div class="ui dropdown selection">
+						<div tabindex="17" class="ui dropdown selection">
 								<input name="size" type="hidden">
 								<div class="default text">...</div>
 								<i class="dropdown icon"></i>
@@ -158,25 +158,25 @@ function registFailed() {
 				</div>
 					<div class="field">
 						<label>特殊疾病</label>
-						<input type="text" placeholder="看到總召就會想捏他的病…" name="disease">
+						<input type="text" placeholder="看到總召就會想捏他的病…" name="disease" tabindex="18">
 					</div>
 				</div>
 				<div class="field">
 					<label>為什麼想來迎新宿營咧?</label>
-					<textarea id="reason_ta" name="reason" placeholder="因為聽說值星很帥呢A_A"></textarea>
+					<textarea id="reason_ta" name="reason" placeholder="因為聽說值星很帥呢A_A" tabindex="20"></textarea>
 				</div>
 				<div class="field">
 					<label>對大學的期待</label>
-					<textarea id="expection_ta" name="expectation" placeholder="Write your expection..."></textarea>
+					<textarea id="expection_ta" name="expectation" placeholder="Write your expection..." tabindex="21"></textarea>
 				</div>
 		</form>
 	</div>
 <div id="button_div">
-	<div id="submit_button" class="ui down button" onclick="$('#registerForm').form('validate form');account.submit();return false;" >
+	<div id="submit_button" class="ui down button" onclick="$('#registerForm').form('validate form');account.submit();return false;" tabindex="22" >
 		<i class="checkmark sign icon"></i>
 		送出
 	</div>
-	<div id="clear_button" class="ui down button" onclick="clearForm();">
+	<div id="clear_button" class="ui down button" onclick="clearForm();" tabindex="23">
 		<i class="trash icon"></i>
 		清除
 	</div>
